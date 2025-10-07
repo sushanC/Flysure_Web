793511e9bf1f7a696cf3b92d8ed94db852853a04
Sushan Achar <sushanvcachar97@gmail.com>
2025-10-07 22:04:13 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with base URL
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL + "/api", // backend base URL
+  baseURL: `${import.meta.env.VITE_BACKEND_URL.replace(/\/+$/, "")}/api`,
   headers: {
     "Content-Type": "application/json",
   },
